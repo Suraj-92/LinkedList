9029652d5eee73e636ce232045e5b2c22edc5608
Suraj-92 <suraj.temkar.92@gmail.com>
2021-02-06 20:52:13 +0530

    LinkedList Created UC-1 Done

---
+++ src/main/java/javapractice/MyNode.java
@@ -0,0 +1,22 @@
+package javapractice;
+
+public class MyNode<K> {
+    private MyNode next;
+    private K key;
+
+    public MyNode(K key){
+        this.key = key;
+        this.next = null;
+    }
+
+    public MyNode getNext() {
+        return next;
+    }
+
+    public void setNext(MyNode next) {
+        this.next = next;
+    }
+
+
+
+}
