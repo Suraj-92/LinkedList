e18d3e3e7c58dd9f7dab1b42c9a4f14bef2d2a0f
Suraj-92 <suraj.temkar.92@gmail.com>
2021-02-07 17:12:54 +0530

    UC-1 Create Queue Updated

--- src/test/java/javapractice/MyQueueTest.java
+++ src/test/java/javapractice/MyQueueTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 public class MyQueueTest {
     @Test
-    public void given3NumbersWhenAddedToStackShouldHaveLastAddesNode() {
+    public void given3NumbersWhenAddedToQueueFromTheFront() {
         MyNode<Integer> myFirstNode = new MyNode<>(70);
         MyNode<Integer> mySecondNode = new MyNode<>(30);
         MyNode<Integer> myThirdNode = new MyNode<>(56);
