d8e78cbe45a7576cbbbff54034e365416cd15f7b
Suraj-92 <suraj.temkar.92@gmail.com>
2021-02-07 16:47:57 +0530

    Pop Operation On Stack is Done UC-2

--- src/test/java/javapractice/MyStackTest.java
+++ src/test/java/javapractice/MyStackTest.java
@@ -17,4 +17,17 @@ public class MyStackTest {
         INode peak = myStack.peak();
         Assertions.assertEquals(myThirdNode,peak);
     }
+    @Test
+    public void given3NumbersInStackWhenPopShouldMatchWithLastAddedNode(){
+        MyNode<Integer> myFirstNode = new MyNode<>(70);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(56);
+        MyStack myStack = new MyStack();
+        myStack.push(myFirstNode);
+        myStack.push(mySecondNode);
+        myStack.push(myThirdNode);
+        INode pop = myStack.pop();
+        myStack.printStack();
+        Assertions.assertEquals(myThirdNode,pop);
+    }
 }
